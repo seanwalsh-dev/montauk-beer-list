f6eea6eff34ebb9805534e3c2f90b015e31517c2
seanwalsh <swalsh804@gmail.com>
2025-11-16 05:26:52 -0700

    added hamburger btn/nav, & @media

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.getElementById("hamburger");
+const hanburgerModal = document.getElementById("hamburger-modal");
+
+console.log("JS IS CONNECTED")
+
+hamburger.addEventListener("click", () => {
+  hanburgerModal.classList.toggle('hidden');
+
+});
+
+console.log("JS IS CONNECTED")
